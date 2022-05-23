0dca3a8b84773dc714401b5a0b4f443889a4d635
alex <spinist.89@mail.ru>
2022-05-23 14:48:46 +0300

    add uslugi

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def html():
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
