f9a735d444283488fdff1459eb7a86939dfc1ce3
alex <spinist.89@mail.ru>
2022-05-24 14:29:54 +0300

    add reviews, pages

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def html():
+def index():
     return render_template('index.html')
+
+@app.route('/etiquette', methods=['POST', 'GET'])
+def etiquette():
+    return render_template('etiquette.html')
+
+@app.route('/contraindications', methods=['POST', 'GET'])
+def contraindications():
+    return render_template('contraindications.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
